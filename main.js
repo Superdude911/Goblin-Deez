dbc808b1326bcb5d4cca561953e6c2e9f8dc6615
Superdude911 <126709296+Superdude911@users.noreply.github.com>
2023-03-01 13:19:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var gameData = {
+    nuts: 0,
+    nutsPerClick: 1,
+    nutsPerClickCost: 10
+}
+function gobbleNuts() {
+    gameData.nuts += gameData.nutsPerClick
+    document.getElementById("nutsGobbled").innerHTML = gameData.nuts + " Nuts Gobbled"
+}
+function buyGoldPerClick() {
+    if (gameData.nuts >= gameData.nutsPerClickCost) {
+        gameData.nuts -= gameData.nutsPerClickCost
+        gameData.nutsPerClick += 1
+        gameData.nutsPerClickCost *= 2
+        document.getElementById("nutsGobbled").innerHTML = gameData.nuts + "Nuts Gobbled"
+        document.getElementById("perClickUpgrade").innerHTML = "Sloppier Tongue (Currently Level " + gameData.nutsPerClick +") Cost: " + gameData.nutsPerClickCost + " Nuts"
+    } 
+}
+var mainGameLoop = window.setInterval(function() {
+    gobbleNuts()
+}, 1000)
+var saveGameLoop = window.setInterval(function() {
+    localStorage.setitem("goblinDeezSave", JSON.stringify(gameData))
+}, 15000)
+var savegame = JSON.parse(localStorage,getItem("goblinDeezSave"))
+if (savegame != null) {
+    gameData= savegame
+}
